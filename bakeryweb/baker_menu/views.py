fe29c43fc1990ba70bc74a39899bdc301bcf4529
Nunes-Bruno <bruno8n@gmail.com>
2023-11-23 15:59:22 -0300

    cadastro clientes bd

--- bakeryweb/baker_menu/views.py
+++ bakeryweb/baker_menu/views.py
@@ -1,8 +1,39 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import MeuFormulario
+import sqlite3
 
 # Create your views here.
 def home(request):
     return render(request, 'painel_inicial/home.html')
 
+
 def cadastro(request):
+    connection = sqlite3.connect('clientes-sqlite.db')
+
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Clientes(
+                   cliente_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT,
+                   telefone TEXT
+                        )
+                    ''')
+    
+    if request.method == 'POST':
+        nome_digitado = request.POST.get('nome')
+        telefone_digitado = request.POST.get('telefone')
+        print(f"nome digitado: {nome_digitado}")
+        print(f"telefone digitado: {telefone_digitado}")
+        cliente_cadastrado = [(nome_digitado, telefone_digitado)]
+        cursor.executemany('''
+                            INSERT INTO Clientes (nome, telefone) VALUES(?,?)
+                            ''', cliente_cadastrado)
+        cursor.execute("SELECT * FROM Clientes ")
+        print(cursor.fetchall())
+        connection.commit()
+        connection.close()
+        return render(request, 'painel_inicial/cadastro.html', {'nome': nome_digitado, 'telefone':telefone_digitado})
     return render(request, 'painel_inicial/cadastro.html')
+
