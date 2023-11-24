62226e44d0068e418d045e35bd527e56355d9925
Nunes-Bruno <bruno8n@gmail.com>
2023-11-24 18:22:20 -0300

    criação da pagina tabela pedidos

--- bakeryweb/baker_menu/forms.py
+++ bakeryweb/baker_menu/forms.py
@@ -3,4 +3,4 @@ from django.core.validators import RegexValidator
 
 class MeuFormulario(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
-    telefone = forms.CharField(max_length=15, validators=[RegexValidator(r'^\+?55?\s?\(?[1-9]{2}\)?\s?9?[0-9]{4}-?[0-9]{4}$', message="Número de celular inválido. O formato aceito é: '999999999'.")])
+    # telefone = forms.CharField(max_length=15, validators=[RegexValidator(r'^\+?55?\s?\(?[1-9]{2}\)?\s?9?[0-9]{4}-?[0-9]{4}$', message="Número de celular inválido. O formato aceito é: '999999999'.")])
